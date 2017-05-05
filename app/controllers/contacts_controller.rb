91147e985ebbccd9e514f374c02fc6a95a8e49b8
yosukearakaki <yosuke1225@gmail.com>
2017-05-05 09:51:47 +0000

    first commit

---
+++ app/controllers/contacts_controller.rb
@@ -0,0 +1,32 @@
+class ContactsController < ApplicationController
+  def new
+    @contact = Contact.new
+  end
+
+  def create
+    @contact = Contact.new(contacts_params)
+    if @contact.save
+      redirect_to root_path, notice: "お問い合わせありがとうございました！"
+    else
+      render 'new'
+    end
+  end
+  
+  def confirm
+    @contact = Contact.new(contacts_params)
+    render :new if @contact.invalid?
+  end
+  
+    def new
+    if params[:back]
+      @contact = Contact.new(contacts_params)
+    else
+      @contact = Contact.new
+    end
+  end
+  
+  private
+    def contacts_params
+      params.require(:contact).permit(:name, :email, :content)
+    end
+end
