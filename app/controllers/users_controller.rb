fcbaa6be26c263e8cb7dbf9014fbb128624e7395
Yosuke Arakaki <yosuke@quipper.com>
2017-06-09 14:56:31 +0900

    curriculum22

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,27 @@
+class UsersController < ApplicationController
+  before_action :set_user, only: [:show]
+
+  def index
+    @users = User.all
+    respond_to do |format|
+      format.html
+      format.js
+    end
+  end
+
+  def show
+    #binding.pry
+    @followers = @user.followers
+    @followed_users = @user.followed_users
+  end
+
+  def edit
+
+  end
+
+  private
+    def set_user
+      @user = User.find(params[:id])
+    end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,18 @@
 Rails.application.routes.draw do
 
+  get 'relationships/create'
+  get 'relationships/destroy'
+
+  resources :relationships, only: [:create, :destroy]
+
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
 
   devise_for :users, controllers: {
     registrations: "users/registrations",
     omniauth_callbacks: "users/omniauth_callbacks"
   }
-  
+  resources :users, only: [:index, :show]
+
   resources :blogs, only: [:index, :new, :create, :edit, :update, :destroy] do
     collection do
       post :confirm
